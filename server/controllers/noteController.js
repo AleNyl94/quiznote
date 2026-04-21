81726ee4cd56329558b41806e563fbdd18262a9c
Alexander Nylander <alexx_94@hotmail.se>
2026-04-21 12:46:59 +0200

    Gjort CRUD för notes i controllern och kommenterat

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -9,27 +9,75 @@ export const noteController = {
    * @param {*} res 
    * @param {*} next 
    */
-  create: async (req, res, next) => {
+  create: async (err, req, res, next) => {
     try {
-      const { title, body } = req.body
-
-      await fetch({
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      await Note.create({
-        title,
-        body,
-        owner: req.session.user.id
-      })
-      res.status(204).send('A new note is created!')
+      const { title, body, owner } = req.body
+      const newNote = new Note({ title, body, owner})
+      const savedNote = await newNote.save()
+      res.status(201).json(savedNote)
     } catch (err) {
-      next (err)
+      res.status(400).json({ error: err.message })
     }
   },
 
-  edit: async
+  /**
+   * Gets the users notes.
+   * 
+   * @param {*} err 
+   * @param {*} req 
+   * @param {*} res 
+   * @param {*} next 
+   */
+  get: async (err, req, res, next) => {
+    try {
+      const notes = await Note.find().populate('owner', 'user.email')
+      res.json(notes)
+    } catch (err) {
+      res.status(500).json({ error: err.message })
+    }
+  },
+
+  /**
+   * Updates the note.
+   * 
+   * @param {*} err 
+   * @param {*} req 
+   * @param {*} res 
+   * @param {*} next 
+   */
+  edit: async (err, req, res, next) => {
+    try {
+      const note = res.locals.note
+      const { title, body } = req.body
+
+      note.title = title
+      note.body = body
+
+      await note.save()
+      res.status(204).json(`Note ${title} is updated successfully`)
+    } catch (err) {
+      res.status(400).json({ error: err.message })
+    }
+  },
+
+  /**
+   * Deletes the note.
+   * 
+   * @param {*} err 
+   * @param {*} req 
+   * @param {*} res 
+   * @param {*} next 
+   * @returns 
+   */
+  delete: async (err, req, res, next) => {
+    try {
+      const note = await Note.findByIdAndDelete(req.params.id)
+      if (!note) {
+        return res.status(404).json({ error: "Note not found" });
+        }
+    } catch (err) {
+      res.status(500).json({ error: err.message })
+    }
+  }
 
 }
