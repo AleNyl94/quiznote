5adf5f94bbe8faa25a4b27546b6d776ea1f3b322
Alexander Nylander <alexx_94@hotmail.se>
2026-05-03 20:20:07 +0200

    Sparar nya tester

---
+++ server/tests/auth.test.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+import request from 'supertest'
+import app from '../app.js'
+
+describe('Test1.1: Authentication', () => {
+  afterAll(async () => {
+    await mongoose.disconnect()
+  })
+
+  const newUser = {
+    username: 'usertest',
+    password: 'secretpassword',
+    email: 'usertest@mail.com'
+    }
+    test('Register a new user', async () => {
+    const response = await request(app)
+    .post('/signup')
+    .send(newUser)
+    
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('Gives a error message if the user is already registered', async () => {
+    const response = await request(app)
+      .post('/signup')
+      .send(newUser)
+
+    expect(response.statusCode).toBe(400)
+  })
+})
