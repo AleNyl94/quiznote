6b3b7a3dba2c5c6909005d822f846085a57c2f96
Alexander Nylander <an225ee@student.lnu.se>
2026-05-12 19:32:07 +0000

    Gjort logiken för quizkortet och stylen

--- client/src/components/quizCard/quizCard.jsx
+++ client/src/components/quizCard/quizCard.jsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
+import { useMemo } from 'react' 
 
-export function QuizCard() {
+export function QuizCard(data, currentIndex, totalQuestions, nextQuestion) {
+
+  const [ selectedAnswer, setSelectedAnswer ] = useState(null)
+  const [ hasAnswered, setHasAnswered ] = useState(null)
+
+  const handleAnswer = (option) => {
+    if (hasAnswered) {
+      return
+    }
+    setSelectedAnswer(option)
+    setHasAnswered(true)
+  }
   
   // Shuffles the answers so the correct answer and vice versa 
   // does not show in the same box for every question
-  const shuffle =React.useMemo(() => {
-    const options = [data.trueAnswer, falseAnswer]
+  const shuffledOptions =React.useMemo(() => {
+    const options = [data.trueAnswer, data.falseAnswer]
     return options.sort(() => Math.random() - 0.5)
   }, [data])
+
+  /**
+   * The quiz card-modal, 
+   */
   return (
     <div className="modal">
       <div className="card-content">
@@ -15,8 +31,21 @@ export function QuizCard() {
         <h2>QUIZ TIME</h2>
         <p className="question-text">{data.question}</p>
         <div className="options">
-
+          {shuffledOptions.map((option, index) => (
+            <button
+              disabled={hasAnswered}
+              className={`optionBtn ${hasAnswered && option === data.trueAnswer ? 'correct' : ''} ${selectedAnswer === option && option !== data.trueAnswer ? 'wrong' : ''}`}
+              onClick={() => handleAnswer(option)}
+            >
+              {option}
+            </button>
+          ))}
         </div>
+        {hasAnswered && (
+          <button className="nextBtn" onClick={nextQuestion}>
+            {currentIndex < totalQuestions - 1 ? 'Next Question' : 'Show Result'}
+          </button>
+        )}
       </div>
     </div>
   )
