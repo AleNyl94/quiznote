89c01efb0dd4a94f67c3d3b8be713d7d61bfd2af
Alexander Nylander <an225ee@student.lnu.se>
2026-05-31 22:13:31 +0000

    Bytte tillbaka til llocalhost

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://cu1066.camp.lnu.se/',
+    baseURL: process.env.CI ? 'http://localhost:3000' : 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -69,20 +69,6 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
-   webServer: [
-    {
-      command: 'cd server && npm start',
-      url: 'http://cu1066.camp.lnu.se/',
-      reuseExistingServer: !process.env.CI,
-      timeout: 120 * 1000,
-    },
-    {
-      command: 'cd client && npm run dev',
-      url: 'http://cu1066.camp.lnu.se/',
-      reuseExistingServer: !process.env.CI,
-      timeout: 120 * 1000,
-    }
   ],
 });
 
